11c32a4eec921a6ace46022db3f7cc198c5366c9
Chris Rohlfs <chris@givealittle.co.nz>
2017-04-26 23:17:35 +1200

    Update packages, add test for failing case

--- __tests__/mopidy.ts
+++ __tests__/mopidy.ts
@@ -1,19 +1,36 @@
-import {createApiObject} from '../src/mopidy';
+import { createApiObject } from '../src/mopidy';
 
 test('api is assigned correctly', () => {
   var api = {
     'core.playback.pause': { description: '', params: [] as any[] },
     'playback.seek': {
-      description: '', 
+      description: '',
       params: [
         { name: 'time', default: null }
-      ] },
+      ]
+    },
     'foo_bar': { description: '', params: [] as any[] }
   }
 
-  var apiObject = createApiObject(api, (method: string, params?: any) => params ? { method, params} : { method });
+  var apiObject = createApiObject(api, (method: string, params?: any) => params ? { method, params } : { method });
 
   expect(apiObject.playback.pause()).toEqual({ method: 'core.playback.pause' });
-  expect(apiObject.playback.seek(55)).toEqual({ method: 'playback.seek', params: { time: 55} });
+  expect(apiObject.playback.seek(55)).toEqual({ method: 'playback.seek', params: { time: 55 } });
   expect(apiObject.fooBar()).toEqual({ method: 'foo_bar' });
+});
+
+test('method with multiple parameters is assigned correctly', () => {
+  var api = {
+    'test.method': {
+      description: '',
+      params: [
+        { name: 'first', default: null },
+        { name: 'second', default: null }
+      ]
+    }
+  }
+
+  var apiObject = createApiObject(api, (method: string, params?: any) => params ? { method, params } : { method });
+
+  expect(apiObject.test.method("one", "two")).toEqual({ method: 'test.method', params: { first: "one", second: "two" } });
 });
